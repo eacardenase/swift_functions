3e28f10030bde534d4b9e756909d564a11d5b33b
Edwin Cardenas <ecardenas@lwolf.com>
2025-03-24 15:04:02 -0500

    Initial commit

--- Contents.swift
+++ Contents.swift
@@ -42,7 +42,7 @@ print(divisionDescriptionFor(numerator: 9.0, denominator: 3.0, withPunctuation: 
 
 var error = "The request failed:"
 
-func appendErrorCode(_ code: Int, toErrorString errorString: inout String) { // inout params cannot have default values
+func appendErrorCode(_ code: Int, toErrorString errorString: inout String) -> Void { // inout params cannot have default values
     if code == 400 {
         errorString += " bad request."
     }
@@ -51,3 +51,121 @@ func appendErrorCode(_ code: Int, toErrorString errorString: inout String) { // 
 appendErrorCode(400, toErrorString: &error)
 
 print(error)
+
+10.isMultiple(of: 2) // everything is an object/struct? Like in Ruby?
+
+func areaOfTriangleWith(base: Double, height: Double) -> Double {
+//    let rectangle = base * height
+    
+    func divide() -> Double {
+        let testVariable = "I'm just a normal string"
+        
+        return rectangle / 2 // rectange is still within scope
+    }
+    
+    let rectangle = base * height // also works
+    // print(testVariable) // is not in the same scope
+    
+    return divide()
+}
+
+print(areaOfTriangleWith(base: 3.0, height: 5.0))
+
+@MainActor func printError() { // this is something interesting about scope
+    print(error)
+}
+
+printError()
+
+func sortedEvenOddNumbers(_ numbers: [Int]) -> (evens: [Int], odds: [Int]) {
+    var evens = [Int]()
+    var odds = [Int]()
+    
+    for number in numbers {
+        if number % 2 == 0 {
+            evens.append(number)
+        } else {
+            odds.append(number)
+        }
+    }
+    
+    return (evens, odds)
+}
+
+let aBunchOfNumbers = [10, 1, 4, 3, 57, 43, 84, 27, 156, 111]
+let theSortedNumbers = sortedEvenOddNumbers(aBunchOfNumbers)
+
+print("The even numbers are: \(theSortedNumbers.evens)\nThe odd numbers are: \(theSortedNumbers.odds)")
+
+func grabMiddleName(fromFullName name: (String, String?, String)) -> String? {
+    return name.1
+}
+
+let middleName = grabMiddleName(fromFullName: ("Edwin", "Alexander", "Cardenas"))
+
+if let theName = middleName {
+    print(theName)
+}
+
+let anotherMiddleName = grabMiddleName(fromFullName: ("Alice", nil, "Ward"))
+
+if let theName = anotherMiddleName {
+    print(theName)
+}
+
+//func greetByMiddleName(fromFullName name: (first: String, middle: String?, last: String)) {
+//    guard let middleName = name.middle else {
+//        print("Hey there!")
+//        
+//        return
+//    }
+//    
+//    print("Hey, \(middleName)")
+//}
+
+func greetByMiddleName(fromFullName name: (first: String, middle: String?, last: String)) {
+    guard let middleName = name.middle, middleName.count > 10 else {
+        print("Hey there!")
+        
+        return
+    }
+    
+    let middleInitial = middleName[middleName.startIndex]
+    
+    print("Hey, \(name.first) \(middleInitial). \(name.last)")
+}
+
+greetByMiddleName(fromFullName: (first: "Edwin", middle: "Alexander", last: "Cardenas"))
+greetByMiddleName(fromFullName: ("Alice", nil, "Ward"))
+greetByMiddleName(fromFullName: ("Alois", "Rumpelstiltskin", "Chaz"))
+
+let evenOddFunction: ([Int]) -> ([Int], [Int]) = sortedEvenOddNumbers
+
+evenOddFunction([1, 2, 3])
+
+func siftBeans(fromGroceryList list: [String]) -> (beans: [String], otherGroceries: [String]) {
+    var beans = [String]()
+    var otherGroceries = [String]()
+    
+    for item in list {
+//        if item.contains("beans") {
+        if item.hasSuffix("beans") {
+            beans.append(item)
+        } else {
+            otherGroceries.append(item)
+        }
+    }
+    
+    return (beans, otherGroceries)
+}
+
+let result = siftBeans(fromGroceryList: [
+    "green beans",
+    "milk",
+    "black beans",
+    "pinto beans",
+    "apples"
+])
+
+result.beans == ["green beans", "black beans", "pinto beans"]
+result.otherGroceries == ["milk", "apples"]
